999486437849733a6b637cf82c0e6d5e8e0ce0e0
RafidMuhymin <rafidmuhymin@gmail.com>
2025-12-14 16:49:14 +0600

    feat: setup path aliasing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { fileURLToPath } from "node:url";
+
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    resolve: {
+      alias: [
+        {
+          find: /^@\/(.*)$/,
+          replacement: fileURLToPath(new URL("./src/$1", import.meta.url)),
+        },
+      ],
+    },
+  },
+});
