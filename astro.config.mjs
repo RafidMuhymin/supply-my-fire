d07c4a62df34827153d92b70a66430c87f88d3e5
RafidMuhymin <rafidmuhymin@gmail.com>
2025-12-14 16:49:59 +0600

    feat: setup tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { fileURLToPath } from "node:url";
 
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
+    plugins: [tailwindcss()],
     resolve: {
       alias: [
         {
