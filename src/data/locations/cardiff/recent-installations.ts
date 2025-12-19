d96d5c33be6abf76ea8d9da322f330e42615d618
RafidMuhymin <rafidmuhymin@gmail.com>
2025-12-20 03:04:23 +0600

    feat: add location RecentInstallations section

---
+++ src/data/locations/cardiff/recent-installations.ts
@@ -0,0 +1,52 @@
+import InstallationImg1 from "@/assets/images/installation-img-1.jpg";
+import InstallationImg2 from "@/assets/images/installation-img-2.jpg";
+import InstallationImg3 from "@/assets/images/installation-img-3.jpg";
+import InstallationImg4 from "@/assets/images/installation-img-4.jpg";
+
+const data = {
+  title: {
+    main: "Recent",
+    highlight: "Cardiff",
+    suffix: "Installations",
+  },
+  installations: [
+    {
+      image: InstallationImg1,
+      badge: "Victorian Terrace",
+      title: "Media Wall Installation",
+      description:
+        "Lorem ipsum dolor sit amet, adipiscing elit, sed eiusmod tempor incididunt ut labore.",
+      location: "Roath",
+    },
+    {
+      image: InstallationImg2,
+      badge: "Victorian terrace transformation",
+      title: "Bespoke Media Wall",
+      description:
+        "Lorem ipsum dolor sit amet, adipiscing elit, sed eiusmod tempor incididunt ut labore.",
+      location: "Pontcanna",
+    },
+    {
+      image: InstallationImg3,
+      badge: "HETAS approved installation",
+      title: "Wood Burning Stove",
+      description:
+        "Lorem ipsum dolor sit amet, adipiscing elit, sed eiusmod tempor incididunt ut labore.",
+      location: "Penarth",
+    },
+    {
+      image: InstallationImg4,
+      badge: "Victorian Terrace",
+      title: "Gas Fire Installation",
+      description:
+        "Lorem ipsum dolor sit amet, adipiscing elit, sed eiusmod tempor incididunt ut labore.",
+      location: "Lisvane",
+    },
+  ],
+  button: {
+    href: "#",
+    text: "See Our Full Portfolio",
+  },
+};
+
+export default data;
