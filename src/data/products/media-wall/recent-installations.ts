3d01bc18a560ffb183d9c4923d4e799305cbade1
RafidMuhymin <rafidmuhymin@gmail.com>
2025-12-22 02:48:28 +0600

    feat: add product RecentInstallation section

---
+++ src/data/products/media-wall/recent-installations.ts
@@ -0,0 +1,48 @@
+import ModernApartment from "@/assets/images/modern-apartment.jpg";
+import InstallationImg2 from "@/assets/images/installation-img-2.jpg";
+import InstallationImg3 from "@/assets/images/installation-img-3.jpg";
+import InstallationImg4 from "@/assets/images/installation-img-4.jpg";
+
+const data = {
+  title: {
+    main: "Recent",
+    highlight: "Cardiff",
+    suffix: "Installations",
+  },
+  installations: [
+    {
+      image: ModernApartment,
+
+      title: "Modern Apartment",
+      description: "Contemporary white finish with Solution SLE150",
+      location: "Cardiff City Centre",
+    },
+    {
+      image: InstallationImg2,
+
+      title: "Victorian Terrace",
+      description: "Wood veneer effect with Gazco eStudio",
+      location: "Pontcanna",
+    },
+    {
+      image: InstallationImg3,
+      badge: "HETAS approved installation",
+      title: "New Build",
+      description: "Grey panel design with Vision VS150",
+      location: "Penarth",
+    },
+    {
+      image: InstallationImg4,
+      badge: "Victorian Terrace",
+      title: "Period Property",
+      description: "Dark finish with Stovax Riva",
+      location: "Roath",
+    },
+  ],
+  button: {
+    href: "#",
+    text: "Request Your Free Design Consultation",
+  },
+};
+
+export default data;
