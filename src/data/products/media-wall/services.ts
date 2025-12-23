3dcfd165e91ae618dc56b43c5b7403cc04a1b9e7
RafidMuhymin <rafidmuhymin@gmail.com>
2025-12-24 04:11:49 +0600

    fix: product Services title and refactor Services sections

--- src/data/products/media-wall/services.ts
+++ src/data/products/media-wall/services.ts
@@ -1,37 +1,30 @@
 import ServiceImg1 from "@/assets/images/service-img-1.png";
 import ServiceImg2 from "@/assets/images/service-img-2.jpg";
 import ServiceImg3 from "@/assets/images/service-img-3.jpg";
-import serviceImg4 from "@/assets/images/service-img-4.jpg";
-import serviceImg5 from "@/assets/images/service-img-5.jpg";
 
 const data = {
-  title: {
-    main: "Our",
-    highlight: "Specialist",
-    suffix: "Services",
-  },
-
+  title: `Other <em>Cardiff Fireplace</em> Installation <em>Services</em>`,
   services: [
     {
       title: "Gas Fire Installation Cardiff",
       description:
         "Gas Safe certified installation throughout Cardiff, balanced flue options",
       image: ServiceImg1,
-      size: "col-span-12",
+      col: "col-span-12",
     },
     {
       title: "Electric Fire Installation Cardiff",
       description:
         "No chimney needed - perfect for modern Cardiff properties and apartments",
       image: ServiceImg2,
-      size: "col-span-7",
+      col: "col-span-7",
     },
     {
       title: "Wood Burning Stove Installation Cardiff",
       description:
         "HETAS approved installation for Cardiff homes, DEFRA approved stoves",
       image: ServiceImg3,
-      size: "col-span-5",
+      col: "col-span-5",
     },
   ],
   button: {
